7c4cd6f1939f10e944903c0089b571a308917868
杨世川 <yscbbc@gmail.com>
2018-09-02 12:17:03 +0800

    项目初始化

---
+++ YYUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYUIKit
+//
+//  Created by 杨世川 on 2018/9/2.
+//  Copyright © 2018年 winwayworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYUIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYUIKit
+//
+//  Created by 杨世川 on 2018/9/2.
+//  Copyright © 2018年 winwayworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
