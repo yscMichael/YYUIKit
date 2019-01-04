90a4e539a6652d1daebc7e40ec6fb73d4cd7f0a0
杨世川 <yscbbc@gmail.com>
2019-01-04 16:13:42 +0800

    运行时研究

--- YYUIKit/运行时/Model/YYTestRuntimeModel.m
+++ YYUIKit/运行时/Model/YYTestRuntimeModel.m
@@ -11,6 +11,56 @@
 #import <objc/runtime.h>
 
 @implementation YYTestRuntimeModel
+#pragma mark - 归档运行时
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super init])
+    {
+        //1.复制属性列表
+        unsigned int outCount = 0;
+        Ivar *varList = class_copyIvarList(self.class, &outCount);
+        //2.for循环对每个成员变量进行操作
+        for (int i = 0; i < outCount; i++)
+        {
+            //3.获取成员变量名,转换成OC的名字
+            Ivar var = varList[i];
+            const char *name = ivar_getName(var);
+            NSString *pName = [NSString stringWithUTF8String:name];
+            //4.通过属性名解档出对应的值
+            id value = [aDecoder decodeObjectForKey:pName];
+            //5.通过KVC方式,把值存储到对应的属性中
+            [self setValue:value forKey:pName];
+        }
+        free(varList);
+    }
+    return self;
+}
+
+#pragma mark - 解档运行时
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    //Ivar: 成员变量
+    //class_copyIvarList: 拷贝参数一的类的所有属性列表出来,第二参数中会被存入列表中的元素数量
+    unsigned int outCount = 0;
+    Ivar *varList = class_copyIvarList(self.class, &outCount);
+    for (int i = 0; i < outCount; i++)
+    {
+        //从列表中获取每一个成员变量
+        Ivar var = varList[i];
+        //获取成员变量的名字
+        const char *name = ivar_getName(var);
+        //C字符串 -> OC字符串
+        NSString *propertyName = [NSString stringWithUTF8String:name];
+        NSLog(@"%@", propertyName);
+        //KVC
+        id value = [self valueForKey:propertyName];
+        //归档
+        [aCoder encodeObject:value forKey:propertyName];
+    }
+    //C语言在用完之后手动进行释放
+    free(varList);
+}
+
 #pragma mark - 拷贝运行时
 - (id)mutableCopyWithZone:(NSZone *)zone
 {
@@ -42,10 +92,6 @@
     free(propertylist);
     return objcopy;
 }
-
-#pragma mark - 归档运行时
-
-
 
 #pragma mark - Getter And Setter
 - (YYTestPerson *)testPerson
